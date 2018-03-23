4d8b6656270f8910b08f0feb03bbad4cbf913de6
Peter Yuskevich <peter@juskiewi.cz>
2018-03-23 16:42:02 +0000

    modify ListBooks to use BooksAPI

---
+++ src/SearchBooks.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+class SearchBooks extends React.Component {
+
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({query: query.trim()})
+        this.props.onSearch(this.state.query)
+    }
+
+
+    render() {
+        return (
+
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link className="close-search" to='/'>Close</Link>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" onChange={(e) => this.updateQuery(e.target.value)} placeholder="Search by title or author"/>
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+            )
+
+
+
+}
+}
+
+export default SearchBooks
